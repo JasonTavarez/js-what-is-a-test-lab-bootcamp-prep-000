10a9f7edbe87c67f5e784d0c7fd74ff08a7ffe6b
Jason Santana <jsantana006@gmail.com>
2019-08-01 01:25:47 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,20 +1,20 @@
 var index = require("../index.js")
 
-describe(what-is-a-test, () => {
+describe('what-is-a-test', () => {
   describe('name', () => {
-    it('returns "Joe"', () => {
-      expect(index.name).toEqual('Joe')
+    it('returns "Susan"', () => {
+      expect(index.name).toEqual('Susan')
     })
   })
 
 
-  describe(height, () => {
+  describe("height", () => {
     it('is less than 40', () => {
       expect(index.height).toBeLessThan(40)
     })
   })
 
-  describe(message, () => {
+  describe("message", () => {
     it('gives the name and height', () => {
       expect(index.message).toInclude(index.name)
       expect(index.message).toInclude(index.height)
